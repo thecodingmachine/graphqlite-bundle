5e863a6f72a1859fd40e58c9c1888c57e0375698
David Négrier <d.negrier@thecodingmachine.com>
2018-12-13 18:22:37 +0100

    Adding first failing unit test

---
+++ Tests/FunctionalTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TheCodingMachine\GraphQL\Controllers\Bundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionalTest extends TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(false);
+    }
+}
