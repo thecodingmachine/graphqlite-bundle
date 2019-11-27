7291a79c9daa6a9a3cba8d922972809b615403ec
Kharhamel <Kharhamel@users.noreply.github.com>
2019-11-27 11:02:43 +0100

    updated InvalidUserPasswordException
    updated InvalidUserPasswordException to correctly extend GraphQLException. Also updated its category.

--- Controller/GraphQL/InvalidUserPasswordException.php
+++ Controller/GraphQL/InvalidUserPasswordException.php
@@ -3,14 +3,13 @@
 
 namespace TheCodingMachine\Graphqlite\Bundle\Controller\GraphQL;
 
-
 use Exception;
-use TheCodingMachine\GraphQLite\GraphQLRuntimeException as GraphQLException;
+use TheCodingMachine\GraphQLite\Exceptions\GraphQLException;
 
 class InvalidUserPasswordException extends GraphQLException
 {
     public static function create(Exception $previous = null)
     {
-        return new self('The provided user / password is incorrect.', 401, $previous);
+        return new self('The provided user / password is incorrect.', 401, $previous, 'Security');
     }
 }
