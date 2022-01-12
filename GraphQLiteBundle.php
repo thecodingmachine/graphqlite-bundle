379aa68df555a4e10c1c2b5660829facb7290fd0
Mistral Oz <2956113+mistraloz@users.noreply.github.com>
2022-01-12 11:27:10 +0100

    getContainerExtension() might add "?ExtensionInterface" as a native return type declaration
    Avoid this error message
    ```
    [info] User Deprecated: Method "Symfony\Component\HttpKernel\Bundle\Bundle::getContainerExtension()" might add "?ExtensionInterface" as a native return type declaration in the future. Do the same in child class "TheCodingMachine\GraphQLite\Bundle\GraphQLiteBundle" now to avoid errors or add an explicit @return annotation to suppress this message.
    ```
    Valid from PHP 7.1 (this bundle required 7.2 and more).

--- GraphQLiteBundle.php
+++ GraphQLiteBundle.php
@@ -20,7 +20,7 @@ class GraphQLiteBundle extends Bundle
         $container->addCompilerPass(new OverblogGraphiQLEndpointWiringPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -1);
     }
 
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new GraphQLiteExtension();
